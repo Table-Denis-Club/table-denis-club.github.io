f2fa29118e0ec89e6cc451a161e957f13433f703
Kang Cheng <kcheng@utilitywarehouse.co.uk>
2018-07-17 20:54:06 -0700

    update

--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/3.2.0/workbox-sw.js");
 
 importScripts(
-  "/precache-manifest.df2aae2e595d61e6fd971e4f4cf06b62.js"
+  "/precache-manifest.8e091864c9031879a597e8360125959a.js"
 );
 
 workbox.core.setCacheNameDetails({prefix: "uwtta"});
